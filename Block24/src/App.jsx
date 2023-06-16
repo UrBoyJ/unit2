ad6ff1ae3e348f3eeaf45ebc5205737d487afd4d
UrBoyJ <jordanbon80@gmail.com>
2023-06-15 18:01:18 -0700

    Completed Block24 Assignment

--- Block24/src/App.jsx
+++ Block24/src/App.jsx
@@ -1,25 +1,34 @@
 import { puppyList } from "./data.js"
 import { useState } from 'react'
+import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
   console.log(useState(puppies))
+  
   return (
-    <>
-      <div {
-        puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
-        })
-      }>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => {
+             return (
+              <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+                {puppy.name}
+              </p>
+            )
+           })}
+           {featPupId && (
+              <div>
+                <h2>{featuredPup.name}</h2>
+                <ul>
+                  <li>Age: {featuredPup.age}</li>
+                  <li>Email: {featuredPup.email}</li>
+                </ul>
+              </div>
+            )}
+    </div>
+  );
 }
 
 export default App
